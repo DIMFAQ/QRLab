71c1108c90026d0d881394bc55ef257503ab87ca
Vian-98 <rizki.favian123@gmail.com>
2025-11-01 22:48:36 +0700

    Perbaikan Seeder

--- qrbe/database/seeders/DatabaseSeeder.php
+++ qrbe/database/seeders/DatabaseSeeder.php
@@ -1,25 +1,19 @@
 <?php
-
 namespace Database\Seeders;
-
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Tambahkan baris ini untuk memanggil seeder login
+        $this->call([
+            InitialUsersSeeder::class
         ]);
     }
 }
